2a2a2aa5bdf102090a145a23d7bac246eef6284b
Akash Aryal <aryal.akash4414@gmail.com>
2020-06-18 09:18:08 -0500

    fixed api issue?

--- app/CustomClasses/Anime/Anime.php
+++ app/CustomClasses/Anime/Anime.php
@@ -1,6 +1,7 @@
 <?php namespace App\CustomClasses\Anime;
 use Jikan\MyAnimeList\MalClient;
 use Jikan\Jikan;
+require base_path().'/vendor/autoload.php';
 
 class Anime extends Jikan
 {
@@ -12,6 +13,11 @@ class Anime extends Jikan
   public function __construct(String $anime){
     $this->jikan = new Jikan;
     $this->search = $this->jikan->AnimeSearch("$anime",1);
+
+   // $this->jikan = new MalClient;
+    //$this->search = $this->jikan->getAnimeSearch(
+    //(new \Jikan\Request\Search\AnimeSearchRequest("$anime"))
+  //);
     $this->firstAnime = $this->search->getResults()[0];
     $this->firstAnimeObject = $this->jikan->Anime($this->firstAnime->getMalId());
   }
